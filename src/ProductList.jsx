ab54d4abd5d48ccfe85a10600c71866c9eb1593b
ABDELHALIM <abdelhalimouammou@gmail.com>
2026-09-22 07:56:30 -0400

    Integrate Redux cart functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,12 +2,21 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from "./CartSlice";
+import CartItem from "./CartItem";
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
+    const CartItems = useSelector((state) => state.cart.items);
+    
+    const calculateTotalQuantity = () => {
+      return CartItems
+        ? CartItems.reduce((total, item) => total + item.quantity, 0)
+        : 0;
+    };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
